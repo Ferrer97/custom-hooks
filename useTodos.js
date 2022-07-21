5fb19af192f106e7ecc5bc423ed7d50ecec01f68
Julio Ferrer <julioferrer@gmail.com>
2022-07-21 14:03:55 -0500

    primer commit

---
+++ useTodos.js
@@ -0,0 +1,52 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = [];
+
+const init = () => {
+  return JSON.parse( localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = () => {
+  const [todos, dispatchTodo] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewtodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+    dispatchTodo(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatchTodo({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatchTodo({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  const todosCount = todos.length;
+
+  const pendingTodoscount = todos.filter( todo => !todo.done).length
+
+
+  return {
+    todos,
+    todosCount,
+    pendingTodoscount,
+    handleNewtodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
